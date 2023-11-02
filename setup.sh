0951d72d1c8774d672aa6b8ed9318f75010831df
Geektzu <alan.zhutao@gmail.com>
2023-11-03 10:44:01 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ pip3 install Faker
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+sudo apt --fix-broken install
+
+
