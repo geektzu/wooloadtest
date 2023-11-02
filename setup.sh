37cbc1b000f34db81370aa9cfcaa094734d7e4f5
Geektzu <alan.zhutao@gmail.com>
2023-11-03 10:19:29 +1300

    fixed set up script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ sudo apt -y update
 sudo apt-get install python3
 sudo apt install python3-pip
 pip3 install selenium
-sudo apt install google-chrome-stable
+pip3 install Faker
+#sudo apt install google-chrome-stable
 
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
