3bf90901d4577a4b7ea78b78479a354944af1889
Geektzu <alan.zhutao@gmail.com>
2023-11-03 09:30:55 +1300

    Added setup bash

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# for installaion at Ubuntu
+
+sudo apt -y update
+sudo apt-get install python3
+sudo apt install python3-pip
+pip3 install selenium
+sudo apt install google-chrome-stable
+
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+
